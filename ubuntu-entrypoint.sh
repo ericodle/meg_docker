e79875974e6b1e54d3ff83d4dd023f465e5546a1
EO <504068014080@tutanota.com>
2025-03-26 17:47:44 +0900

    revert

--- ubuntu-entrypoint.sh
+++ ubuntu-entrypoint.sh
@@ -15,10 +15,14 @@ west update
 # Install required Python packages
 pip install --no-cache-dir natsort pyelftools junitparser pytest psutil tabulate pyserial anytree ply
 
+# Move into the test directory
+cd nucleof429zi_cicd
+
 # Configure GitHub Actions Runner (use actual GitHub repository URL and token)
 if [ ! -d "/actions-runner/.runner" ]; then
-  /actions-runner/config.sh --url "$GITHUB_URL" --token "${GITHUB_RUNNER_TOKEN}" --labels ubuntu-runner
+  /actions-runner/config.sh --url https://github.com/ericodle/nucleof429zi_cicd --token ARL35Q3OMETPSK36H3DMDOLH4J3AW --labels ubuntu-runner
 fi
 
 # Run the GitHub Actions Runner
 /actions-runner/run.sh
+
